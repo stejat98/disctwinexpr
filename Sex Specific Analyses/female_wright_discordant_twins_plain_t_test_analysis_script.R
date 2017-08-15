a0720c7043d3a5d65f242035939fbbe8f615d45b
Sivateja Tangirala <sivatejatangirala@Sivatejas-MacBook-Pro.local>
2017-08-15 16:26:41 -0500

    initial commit

---
+++ Sex Specific Analyses/female_wright_discordant_twins_plain_t_test_analysis_script.R
@@ -0,0 +1,181 @@
+# load package
+library(GEOquery)
+library(stringr)
+library(rmeta)
+require(data.table)
+
+
+## read table with sample info and expression values for each twin sample
+twinsExpression <- readRDS('/groups/patel/dbgap/twins_depression/PhenoGenotypeFiles/ExpressionFiles/twins_expression.RDS')
+
+## subset data frame to get only FEMALE MZ pairs
+MZ_twinsExpression_df <- subset(twinsExpression, TWIN > 0 & Sex == 2)
+
+## subset to only get twins with MDD
+MZ_MDD_twinsExpression_df <- subset(MZ_twinsExpression_df, MDD == 1)
+
+## subset to only get twins without MDD/ controls
+MZ_Healthy_twinsExpression_df <- subset(MZ_twinsExpression_df, MDD == 0)
+
+## get list of discordant twins
+discordant_twin_pair_ids <-  intersect(MZ_MDD_twinsExpression_df$TWIN,MZ_Healthy_twinsExpression_df$TWIN)
+
+#mzdiscordatwinids <- c(2,3,18,147,157,167,191,201,202,266,283,285,289,298,303,307,314,315,332,359,361,366,377,385,423,457,480,501,532,597,642,643,675,713)
+#inputmzdiscordaids <- mzdiscordatwinids[-c(6,24,32)]
+
+
+#temp_df_MZ <- subset(twinsExpression, TWIN == 332)
+
+
+## get annotation information for GPL13667 platform
+gpl13667info <- getGEO('GPL13667',AnnotGPL = TRUE)
+str(gpl13667info)
+
+## Extract annotation table for GPL13667 platform
+gpl13667infodf <- gpl13667info@dataTable@table
+
+
+
+## Extract microarray probe ids and assign them to a vector
+probe_id_gsewright_discordant_general <- as.character(t(gpl13667infodf$ID))
+
+probe_id_gsewright_discordant_general <- unlist(lapply(as.character(t(gpl13667infodf$ID)), function(a){paste('x',a,sep="")}))
+## Extract Entrez Gene IDs and assign them to a vector
+gene_id_gsewright_discordant_general <- as.character(t(gpl13667infodf$"Entrez Gene"))
+
+
+
+
+
+
+## subset to only include discordant twin samples
+discordant_MZ_MDD_twinsExpression_df <- MZ_MDD_twinsExpression_df[match(discordant_twin_pair_ids,MZ_MDD_twinsExpression_df$TWIN),]
+
+discordant_MZ_Healthy_twinsExpression_df <- MZ_Healthy_twinsExpression_df[match(discordant_twin_pair_ids,MZ_Healthy_twinsExpression_df$TWIN),]
+
+## female affected group of samples
+toneparameter <- discordant_MZ_MDD_twinsExpression_df[,grep('^x', colnames(twinsExpression))]
+## female unaffected group of samples (control)
+ttwoparameter <- discordant_MZ_Healthy_twinsExpression_df[,grep('^x', colnames(twinsExpression))]
+
+## function that assigns output values (mean difference, p-value, standard error) from t.test function into a data frame format
+tloop <- function(a,b) { 
+  
+  ttest <- t.test(a,b, paired = TRUE)
+  
+  ttestoutput <- data.frame( meandiff = ttest$estimate, pvalue = ttest$p.value, (stanerror = (ttest$conf.int[2] - ttest$estimate)/1.96))
+  
+  return(ttestoutput)
+  
+}
+## runs tloop() over all the columns in both twin groups
+twinttestoutputdf <- sapply(c(1: ncol(toneparameter)) , function(a) tloop(toneparameter[,a], ttwoparameter[,a]))
+
+meandiffwright_discordant_general <- twinttestoutputdf[1,] # vector of mean differences for gsewright_discordant_general study twins data
+pvaluewright_discordant_general <- twinttestoutputdf[2,]    # vector of pvalue
+stanerrorwright_discordant_general <- twinttestoutputdf[3,]     # vector of standard error values
+
+## get corresponding probe-level ids
+gsewright_discordant_generalprobelabels <- colnames(toneparameter)
+seconditergplwright_discordant_generaldf <- cbind(gsewright_discordant_generalprobelabels,meandiffwright_discordant_general,pvaluewright_discordant_general,stanerrorwright_discordant_general)
+
+## reformat problematic probe_ids
+temp_1_df <- gsub("xAFFX-HUMGAPDH/M33197_3_","xAFFX-HUMGAPDH/M33197_3_at",seconditergplwright_discordant_generaldf[,1])
+temp_1_df <- gsub("xAFFX-HUMGAPDH/M33197_5_","xAFFX-HUMGAPDH/M33197_5_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-HUMGAPDH/M33197_M_","xAFFX-HUMGAPDH/M33197_M_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-HUMISGF3A/M97935_3","xAFFX-HUMISGF3A/M97935_3_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-HUMISGF3A/M97935_5","xAFFX-HUMISGF3A/M97935_5_at",temp_1_df)
+temp_1_df <- gsub("^xAFFX-HUMISGF3A/M97935_M$","xAFFX-HUMISGF3A/M97935_MA_at",temp_1_df)
+temp_1_df <- gsub("^xAFFX-HUMISGF3A/M97935_M.1$","xAFFX-HUMISGF3A/M97935_MB_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC03_a","xAFFX-Nonspecific-GC03_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC04_a","xAFFX-Nonspecific-GC04_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC05_a","xAFFX-Nonspecific-GC05_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC06_a","xAFFX-Nonspecific-GC06_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC07_a","xAFFX-Nonspecific-GC07_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC08_a","xAFFX-Nonspecific-GC08_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC09_a","xAFFX-Nonspecific-GC09_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC10_a","xAFFX-Nonspecific-GC10_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC11_a","xAFFX-Nonspecific-GC11_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC12_a","xAFFX-Nonspecific-GC12_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC13_a","xAFFX-Nonspecific-GC13_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC14_a","xAFFX-Nonspecific-GC14_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC15_a","xAFFX-Nonspecific-GC15_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC16_a","xAFFX-Nonspecific-GC16_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC17_a","xAFFX-Nonspecific-GC17_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC18_a","xAFFX-Nonspecific-GC18_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC19_a","xAFFX-Nonspecific-GC19_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC20_a","xAFFX-Nonspecific-GC20_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC21_a","xAFFX-Nonspecific-GC21_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC22_a","xAFFX-Nonspecific-GC22_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC23_a","xAFFX-Nonspecific-GC23_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC24_a","xAFFX-Nonspecific-GC24_at",temp_1_df)
+temp_1_df <- gsub("xAFFX-Nonspecific-GC25_a","xAFFX-Nonspecific-GC25_at",temp_1_df)
+
+
+
+
+#input_changing_id <- seconditergplwright_discordant_generaldf[,1][which(temp_check == FALSE)][10:30]
+
+# <- is.element(seconditergplwright_discordant_generaldf[,1],intersect(probe_id_gsewright_discordant_general,seconditergplwright_discordant_generaldf[,1]))
+
+
+seconditergplwright_discordant_generaldf[,1] <- temp_1_df
+
+## transform data into compatible format for meta-analysis of probe-level values and bind entrez geneids (from annotation table) to t-test results data frame
+thirditeringpl13667probeorderdf <- seconditergplwright_discordant_generaldf[match(probe_id_gsewright_discordant_general,seconditergplwright_discordant_generaldf[,1]),]
+thirditeringpl13667probeorderdf <- as.data.frame(thirditeringpl13667probeorderdf)
+thirditeringpl13667probeorderdf$gsewright_discordant_generalprobelabels <- as.character(thirditeringpl13667probeorderdf$gsewright_discordant_generalprobelabels)
+thirditeringpl13667probeorderdf$meandiffwright_discordant_general <- as.numeric(as.character(thirditeringpl13667probeorderdf$meandiffwright_discordant_general))
+thirditeringpl13667probeorderdf$pvaluewright_discordant_general <- as.numeric(as.character(thirditeringpl13667probeorderdf$pvaluewright_discordant_general))
+thirditeringpl13667probeorderdf$stanerrorwright_discordant_general <- as.numeric(as.character(thirditeringpl13667probeorderdf$stanerrorwright_discordant_general))
+gene_id_gsewright_discordant_general <- as.data.frame(gene_id_gsewright_discordant_general)
+forthiterplusgeneiddf <- cbind(thirditeringpl13667probeorderdf[,1],gene_id_gsewright_discordant_general, thirditeringpl13667probeorderdf[,2], thirditeringpl13667probeorderdf[,3],thirditeringpl13667probeorderdf[,4])
+colnames(forthiterplusgeneiddf) <- c("probeid","geneid","meandiff","pval","stanerror")
+## remove the 1708 probes the study didn't use (or for which there are no values recorded)
+fifthiterplusgeneiddf <- forthiterplusgeneiddf[-which(is.na(forthiterplusgeneiddf$meandiff)),]
+
+
+## convert data frame into data table format
+dttemp <- data.table(fifthiterplusgeneiddf)
+## split rows with multiple geneids corresponding to one probe id into separate rows
+dttemp.out <- dttemp[, list(probeid=probeid , geneid = unlist(strsplit(as.character(geneid), "///")), meandiff =meandiff,stanerror=stanerror,pval=pval), by=1:nrow(dttemp)]
+## convert into data frame format
+secondtempwright_discordant_generaldf <- data.frame(lapply(dttemp.out, as.character), stringsAsFactors=FALSE)
+## trim whitespace in gene id vector
+secondtempwright_discordant_generaldf$geneid <- str_trim(secondtempwright_discordant_generaldf$geneid)
+## get data frame with frequencies of geneids
+testinputloop <- as.data.frame(table(secondtempwright_discordant_generaldf$geneid))
+## get unique geneids
+testinputloop$Var1
+
+
+## define meta-analytic function
+getgenelevelidvalfunct <- function(a)
+{
+  secondtempwright_discordant_generaldf.sub1 <- subset(secondtempwright_discordant_generaldf, geneid == as.character(a) , select = c(meandiff, stanerror,geneid))
+  metasumtest1 <- meta.summaries(as.numeric(secondtempwright_discordant_generaldf.sub1$meandiff), as.numeric(secondtempwright_discordant_generaldf.sub1$stanerror), method = "fixed", logscale = FALSE)
+  assign(paste("tempgenevector",a,sep=""), c(metasumtest1$summary,metasumtest1$se.summary,pnorm(abs(metasumtest1$test[1]), lower.tail=F)*2))
+  
+}
+
+## apply over all geneids 
+genelevelvalgsewright_discordant_general <- lapply(testinputloop$Var1,getgenelevelidvalfunct)
+## convert to data frame
+genelevelvalgsewright_discordant_generaldf <- as.data.frame(genelevelvalgsewright_discordant_general)
+## transpose data frame
+genelevelvaluegsewright_discordant_generaldf <- t(genelevelvalgsewright_discordant_generaldf)
+colnames(genelevelvaluegsewright_discordant_generaldf) <- c("meandiff", "stanerror","pval")
+genelevelvaluegsewright_discordant_generaldf <- as.data.frame(genelevelvaluegsewright_discordant_generaldf)
+
+## FDR-correction of p-values for each gene
+FDR_GSEwright_discordant_general <- p.adjust(genelevelvaluegsewright_discordant_generaldf$pval, method="fdr")
+## column bind FDR values column to gene-level value results data frame
+genelevelvaluegsewright_discordant_generaldf <- cbind(GENEID=testinputloop$Var1,genelevelvaluegsewright_discordant_generaldf,FDR_GSEwright_discordant_general )
+## remove non-entrez gene ids
+genelevelvaluegsewright_discordant_generaldf <- genelevelvaluegsewright_discordant_generaldf[-c(1,19286:19311),]
+## find # of FDR significant genes
+sum(genelevelvaluegsewright_discordant_generaldf[,5] < 0.05)
+
+## save data frame to your desired sub-directory 
+#saveRDS(genelevelvaluegsewright_discordant_generaldf, '/home/st320/second_iter_gse_genelevel_dfs/female_genelevelvalue_plain_t_test_gsewright_discordant_generaldf.RDS')
+
---
+++ Sex Specific Analyses/male_MEXP1425_discordant_twin_analysis_script.R
@@ -0,0 +1,158 @@
+## sex-specific analyses 
+
+## Install & Load necesssary packages
+#source("http://www.bioconductor.org/biocLite.R")
+#biocLite("GEOquery")
+require(gdata)
+library(Biobase)
+library(GEOquery)
+library('ArrayExpress')
+require(data.table)
+library(rmeta)
+
+
+
+mexp1425 = getAE("E-MEXP-1425", type = "full")
+## Build a an ExpressionSet from the raw data
+MEXP1425raw = ae2bioc(mageFiles = mexp1425)
+## Build a an ExpressionSet from the processed data
+cnames = getcolproc(mexp1425)
+MEXP1425proc = procset(mexp1425, cnames[2])
+
+## used to extract phenotype/disease diagnosis corresponding to each twin sample
+phenodiagnosis_MEXP1425 <- MEXP1425proc@phenoData@data$Characteristics..disease.
+## used to find which two samples should use from triplets -- not using "Fat twin 9 slimmer"
+add_pheno <- MEXP1425proc@phenoData@data$Sample.Name ## used to find which two samples should use from triplets -- not using "Fat twin 9 slimmer"
+
+twp_num <- MEXP1425proc@phenoData@data$Characteristics..family.relationship.
+sex_group <- MEXP1425proc@phenoData@data$Characteristics..sex. 
+Age <- MEXP1425proc@phenoData@data$Characteristics..age.
+
+## create vector of 0s and 1st indicating phenotype status
+Healthy0.Obesity1 <- vector(mode="integer", length=27)
+Healthy0.Obesity1[grep('normal', phenodiagnosis_MEXP1425)] <- 0
+Healthy0.Obesity1[grep('obesity', phenodiagnosis_MEXP1425)] <- 1
+
+## extract expression table
+gsm_tables_MEXP1425 <- exprs(MEXP1425proc)
+gsm_tables_MEXP1425 <- as.data.frame(gsm_tables_MEXP1425)
+
+## Table with information (sex, age, twin pair number, etc.) for each sample
+Sample_Info <- cbind(Colnames_eset= colnames(gsm_tables_MEXP1425),Healthy0.Obesity1, add_pheno,Phenodiagnosis=phenodiagnosis_MEXP1425,TWP_NUM = gsub('monozygotic twin pair ','',twp_num),Sex_Group = sex_group,Age)
+Sample_Info <- as.data.frame(Sample_Info)
+
+# remove third twin in TWP 9 as it consist of a triplet
+Sample_Info <- Sample_Info[-14,]
+
+#saveRDS(Sample_Info, '/home/st320/MEXP_1425_sample_info.RDS')
+
+## look at only male samples
+Male_Sample_Info <- subset(Sample_Info, Sex_Group == 'male')
+
+## separate Sample_Info (Male) table into two tables: one only for affected samples and one for the unaffected
+healthy_samples <- subset(Male_Sample_Info, Healthy0.Obesity1 ==0)
+diseased_samples <- subset(Male_Sample_Info, Healthy0.Obesity1 == 1)
+
+
+diseased_samples <- diseased_samples[match(healthy_samples$TWP_NUM,diseased_samples$TWP_NUM),]
+
+## male affected group of samples
+tone <- gsm_tables_MEXP1425[,match(diseased_samples$Colnames_eset,colnames(gsm_tables_MEXP1425))]
+## male unaffected group of samples (control)
+ttwo <- gsm_tables_MEXP1425[,match(healthy_samples$Colnames_eset,colnames(gsm_tables_MEXP1425))]
+
+
+
+## get annotation information for GPL570 platform
+gpl570info <- getGEO('GPL570',AnnotGPL = TRUE)
+str(gpl570info)
+
+## Extract annotation table for GPL570 platform
+gpl570infodf <- gpl570info@dataTable@table
+
+
+
+## Extract microarray probe ids and assign them to a vector
+probe_id_MEXP1425 <- as.character(t(gpl570infodf$ID))
+## Extract Entrez Gene IDs and assign them to a vector
+gene_id_MEXP1425 <- as.character(t(gpl570infodf$"Gene ID"))
+
+###  transform data into format compatible with the (paired) t.test function
+toneparameter <- t(tone)
+ttwoparameter <- t(ttwo)
+
+## function that assigns output values (mean difference, p-value, standard error) from t.test function into a data frame format
+tloop <- function(a,b) { 
+  
+  ttest <- t.test(a,b, paired = TRUE)
+  
+  ttestoutput <- data.frame( meandiff = ttest$estimate, pvalue = ttest$p.value, (stanerror = (ttest$conf.int[2] - ttest$estimate)/1.96))
+  
+  return(ttestoutput)
+  
+}
+## runs tloop() -- performs t-test --  over all the columns between both twin groups
+twinttestoutputdf <- sapply(c(1: ncol(toneparameter)) , function(a) tloop(toneparameter[,a], ttwoparameter[,a]))
+
+meandiffMEXP1425 <- twinttestoutputdf[1,] # vector of mean differences for MEXP1425 study twins data
+pvalueMEXP1425 <- twinttestoutputdf[2,]    # vector of pvalue
+stanerrorMEXP1425 <- twinttestoutputdf[3,]     # vector of standard error values
+
+## extract probe labels
+MEXP1425probelabels <- rownames(tone)
+
+## column bind probe labels with t-test results
+seconditergplMEXP1425df <- cbind(MEXP1425probelabels,meandiffMEXP1425,pvalueMEXP1425,stanerrorMEXP1425)
+
+## transform data into compatible format for meta-analysis of probe-level values and bind entrez geneids (from annotation table) to t-test results data frame
+thirditeringpl570probeorderdf <- seconditergplMEXP1425df[match(probe_id_MEXP1425,seconditergplMEXP1425df[,1]),]
+thirditeringpl570probeorderdf <- as.data.frame(thirditeringpl570probeorderdf)
+thirditeringpl570probeorderdf$MEXP1425probelabels <- as.character(thirditeringpl570probeorderdf$MEXP1425probelabels)
+thirditeringpl570probeorderdf$meandiffMEXP1425 <- as.numeric(as.character(thirditeringpl570probeorderdf$meandiffMEXP1425))
+thirditeringpl570probeorderdf$pvalueMEXP1425 <- as.numeric(as.character(thirditeringpl570probeorderdf$pvalueMEXP1425))
+thirditeringpl570probeorderdf$stanerrorMEXP1425 <- as.numeric(as.character(thirditeringpl570probeorderdf$stanerrorMEXP1425))
+gene_id_MEXP1425 <- as.data.frame(gene_id_MEXP1425)
+forthiterplusgeneiddf <- cbind(thirditeringpl570probeorderdf[,1],gene_id_MEXP1425, thirditeringpl570probeorderdf[,2], thirditeringpl570probeorderdf[,3],thirditeringpl570probeorderdf[,4])
+colnames(forthiterplusgeneiddf) <- c("probeid","geneid","meandiff","pval","stanerror")
+
+## convert data frame into data table format
+dttemp <- data.table(forthiterplusgeneiddf)
+## split rows with multiple geneids corresponding to one probe id into separate rows
+dttemp.out <- dttemp[, list(probeid=probeid , geneid = unlist(strsplit(as.character(geneid), "///")), meandiff =meandiff,stanerror=stanerror,pval=pval), by=1:nrow(dttemp)]
+## convert into data frame format
+secondtempMEXP1425df <- data.frame(lapply(dttemp.out, as.character), stringsAsFactors=FALSE)
+
+## get data frame with frequencies of geneids
+testinputloop <- as.data.frame(table(secondtempMEXP1425df$geneid))
+## get unique geneids
+testinputloop$Var1
+
+## define meta-analytic function
+getgenelevelidvalfunct <- function(a)
+{
+  secondtempMEXP1425df.sub1 <- subset(secondtempMEXP1425df, geneid == as.character(a) , select = c(meandiff, stanerror,geneid))
+  metasumtest1 <- meta.summaries(as.numeric(secondtempMEXP1425df.sub1$meandiff), as.numeric(secondtempMEXP1425df.sub1$stanerror), method = "fixed", logscale = FALSE)
+  assign(paste("tempgenevector",a,sep=""), c(metasumtest1$summary,metasumtest1$se.summary,pnorm(abs(metasumtest1$test[1]), lower.tail=F)*2))
+  
+}
+
+## apply over all geneids 
+genelevelvalMEXP1425 <- lapply(testinputloop$Var1,getgenelevelidvalfunct)
+## convert to data frame
+genelevelvalMEXP1425df <- as.data.frame(genelevelvalMEXP1425)
+## transpose data frame
+genelevelvalueMEXP1425df <- t(genelevelvalMEXP1425df)
+colnames(genelevelvalueMEXP1425df) <- c("meandiff", "stanerror","pval")
+genelevelvalueMEXP1425df <- as.data.frame(genelevelvalueMEXP1425df)
+
+## FDR-correction of p-values for each gene
+FDR_MEXP1425 <- p.adjust(genelevelvalueMEXP1425df$pval, method="fdr")
+
+## column bind FDR values column to gene-level value results data frame
+genelevelvalueMEXP1425df <- cbind(GENEID=testinputloop$Var1,genelevelvalueMEXP1425df,FDR_MEXP1425 )
+
+## change column name to FDR
+colnames(genelevelvalueMEXP1425df)[5] <- "FDR"
+
+## save data frame to your desired sub-directory 
+#saveRDS(genelevelvalueMEXP1425df, '/home/st320/second_iter_gse_genelevel_dfs/male_genelevelvalue_plain_t_test_MEXP1425df.RDS')
